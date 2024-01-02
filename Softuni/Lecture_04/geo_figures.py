9730d50a336c496a34a819003ea52029442ec786
plamenshopov <plamenshopov@gmail.com>
2024-01-02 22:16:32 +0200

    file renamed

--- Softuni/Lecture_04/geo_figures.py
+++ Softuni/Lecture_04/geo_figures.py
@@ -4,7 +4,7 @@ figure_type = str(input("What type of figure do you have: "))
 
 if figure_type == "square":
     square_side_length = float(input("Please enter the square side length: "))
-    print(f"The area of the square is {square_side_length * square_side_length} ")
+    print(f"The area of the square is {square_side_length * square_side_length:.3f} ")
 elif figure_type == "rectangle":
     rectangle_side_one = float(input("Please enter the first rectangle side length: "))
     rectangle_side_two = float(input("Please enter the second rectangle side length: "))
@@ -16,4 +16,5 @@ elif figure_type == "circle":
     print(f"The area of the circle is {circle_area:.3f}")
 elif figure_type == "triangle":
     triangle_side_one = float(input("Please enter the first triangle side length: "))
-    triangle_side_two = float(input("Please enter the first triangle side length: "))
+    triangle_side_two = float(input("Please enter the second triangle side length: "))
+    print(f"The area of the triangle is {(triangle_side_one * triangle_side_two) / 2:.3f}")
